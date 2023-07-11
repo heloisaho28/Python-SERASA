adbf23b8b8019857af336d114e4c1f7599e284f2
Heloísa Helena de Oliveira <121984471+heloisaho28@users.noreply.github.com>
2023-07-10 22:18:45 -0300

    Final lista Beecrowd

---
+++ Lista 1/1047.py
@@ -0,0 +1,18 @@
+hInicio, mInicio, hFim, mFim = str(input()).split()
+hInicio = int(hInicio)
+mInicio = int(mInicio)
+hFim = int(hFim)
+mFim = int(mFim)
+
+ini = hInicio * 60 + mInicio
+fim = hFim * 60 + mFim
+
+if ini < fim:
+    hTempo = (fim - ini) // 60
+    mTempo = (fim - ini) % 60
+else:
+    hTempo = ((1440 - ini) + fim) // 60
+    mTempo = ((1440 - ini) + fim) % 60
+
+
+print(f'O JOGO DUROU {hTempo} HORA(S) E {mTempo} MINUTO(S)')
